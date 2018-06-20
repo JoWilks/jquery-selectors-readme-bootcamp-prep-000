a348e9a57b30c9b045bcac3bab1255148523ecbf
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-20 14:23:06 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,15 +1,5 @@
 // declare your functions here...
 
-<<<<<<< HEAD
-/*function paragraphSelector() {
-  var parSelect = $('p');
-  var allPar = [];
-  for (let i = 0; i<parSelect.length;i++) {
-  var currPar = parSelect[i].innerHTML
-  allPar.push(currPar);
-  }
-}   */
-
 function paragraphSelector() {
   return $('p')
 }
@@ -28,7 +18,8 @@ function divSelector() {
 
 function firstListItem() {
  return $('#pic-list li:first-child')
-=======
+}
+
 function paragraphSelector() {
   $('p')
 }
@@ -47,5 +38,4 @@ function divSelector() {
 
 function firstListItem() {
   $('ul .pic-list:first-child')
->>>>>>> b3bb4c40a36530845bdae28cdc1db25db340b0d2
 }
